9baa39562247eb730688a7e4f703ac14dc1e1098
denni582 <dennis.boute@tvh.com>
2023-06-12 15:13:53 +0200

    changed index.js

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,13 @@
+const { onRequest } = require('firebase-functions/v2/https');
+const corsAnywhere = require('cors-anywhere');
+const cors = require('cors');
+
 const corsServer = corsAnywhere.createServer({
     originWhitelist: [
-        'http://localhost:3000',
-        'http://localhost:5000',
-        'https://littlemomster.be', //call from
-        'https://rest.pay.nl' //call to
+      'http://localhost:3000',
+      'http://localhost:5000',
+      'https://littlemomster.be',
+      'https://rest.pay.nl'
     ],
     requireHeader: ['origin', 'x-requested-with'],
     removeHeaders: ['cookie', 'cookie2']
@@ -13,6 +17,6 @@ const corsHandler = cors({ origin: true });
 
 exports.proxy = onRequest((request, response) => {
     corsHandler(request, response, () => {
-        corsServer.emit('request', request, response);
+      corsServer.emit('request', request, response);
     })
 });
