211e2bdc1c216a304957b93979d6edb59cea9a51
denni582 <dennis.boute@tvh.com>
2023-06-12 12:52:56 +0200

    changed index.js

--- functions/index.js
+++ functions/index.js
@@ -1,15 +1,9 @@
-const { onRequest } = require('firebase-functions/v2/https');
-const corsAnywhere = require('cors-anywhere');
-const cors = require('cors');
-
 const corsServer = corsAnywhere.createServer({
     originWhitelist: [
-      'http://localhost:3000',
-      'http://localhost:5000',
-      'https://portfolio-ravenous.web.app',
-      'https://portfolio-ravenous.firebaseapp.com',
-      'https://test-my-api-endpoint.web.app',
-      'https://test-my-api-endpoint.firebaseapp.com'
+        'http://localhost:3000',
+        'http://localhost:5000',
+        'https://littlemomster.be', //call from
+        'https://rest.pay.nl' //call to
     ],
     requireHeader: ['origin', 'x-requested-with'],
     removeHeaders: ['cookie', 'cookie2']
@@ -19,6 +13,6 @@ const corsHandler = cors({ origin: true });
 
 exports.proxy = onRequest((request, response) => {
     corsHandler(request, response, () => {
-      corsServer.emit('request', request, response);
+        corsServer.emit('request', request, response);
     })
 });
